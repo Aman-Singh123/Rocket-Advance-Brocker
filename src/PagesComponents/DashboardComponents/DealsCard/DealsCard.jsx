1e8bc44874af1a9526245388ad67d0bb67e5c59e
Aman-Singh123 <amansingh701173@gmail.com>
2025-06-13 21:59:24 +0530

    first commit

---
+++ src/PagesComponents/DashboardComponents/DealsCard/DealsCard.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./DealsCard.scss";
+
+export default function DealsCard(
+  {
+    cardImgLeft,
+    dealsText,
+    DealsValue,
+    dateValue,
+    advanceText,
+    fontLg,
+    smallText,
+    show,
+  },
+  props
+) {
+  return (
+    <div className="dealsCard whiteCard" {...props}>
+      <div className="dealsCard_imgSec">
+        <span className="dealsCard_imgSec_left">
+          {cardImgLeft && <img src={cardImgLeft} alt="Icon" />}
+        </span>
+        <span className="dealsCard_imgSec_right">
+          {/* {dots && (
+            <span className="dealsCard_imgSec_dots">
+              <ThreeDots />
+            </span>
+          )} */}
+          <h3>{dealsText}</h3>
+          {advanceText && <h3 style={{ marginBottom: 12 }}>{advanceText}</h3>}
+        </span>
+      </div>
+      <p className="smallText">{smallText}</p>
+      <h2 className={fontLg ? "fontLg" : ""}>
+        {!show ? "$" : null}
+        {DealsValue}
+      </h2>
+      <div className="dealsCard_date">{dateValue} </div>
+    </div>
+  );
+}
